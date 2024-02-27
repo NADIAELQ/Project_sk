f455b631839334c899a79a82f3187ba2f95c7427
EL QADI NADIA <128300612+NADIAELQ@users.noreply.github.com>
2024-02-27 22:28:30 +0000

    Add files via upload

---
+++ equipment/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import EquipmentType, VehicleType
+
+
+# Register your models here.
+
+
+admin.site.register(EquipmentType)
+admin.site.register(VehicleType)
