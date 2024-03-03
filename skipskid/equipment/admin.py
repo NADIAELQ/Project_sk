f441b04fd143f34ba26bcbc44630505f54068822
nadia <nadiaelqadi0660@gmail.com>
2024-03-03 19:19:14 +0000

    commit#

--- skipskid/equipment/admin.py
+++ skipskid/equipment/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from django.utils.translation import gettext_lazy as _
 from .models import EquipmentType, VehicleType
 
 
@@ -7,4 +7,32 @@ from .models import EquipmentType, VehicleType
 
 
 admin.site.register(EquipmentType)
+class EquipmentTypeAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+    ordering = ['name']
+
+    def has_add_permission(self, request):
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        return True
+
+    def has_delete_permission(self, request, obj=None):
+        return True
+
+
 admin.site.register(VehicleType)
+class VehicleTypeAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+    ordering = ['name']
+
+    def has_add_permission(self, request):
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        return True
+
+    def has_delete_permission(self, request, obj=None):
+        return True
