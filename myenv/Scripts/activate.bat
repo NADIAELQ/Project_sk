99b69c4ab4b9357158b4cd79986e17081b3b84b8
noura <nourasegaoui@gmail.com>
2024-04-21 12:17:42 +0200

    21/04 from front

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Noura\Desktop\silyabusiness\SkipSkid\myenv
+set VIRTUAL_ENV=C:\Users\Noura\Desktop\silya\SkipSkid\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
