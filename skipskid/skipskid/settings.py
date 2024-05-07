7a9b2204b8e498cd9bc91a1239d8333a5f179128
nadia <nadiaelqadi0660@gmail.com>
2024-05-08 00:30:35 +0100

    style

--- skipskid/skipskid/settings.py
+++ skipskid/skipskid/settings.py
@@ -113,8 +113,9 @@ ROOT_URLCONF = 'skipskid.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'),
-                 'C:\\Users\\Noura\\Desktop\\skipskid_20-04\\skipskid\\templates\\front'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')
+                #  'C:\\Users\\Noura\\Desktop\\skipskid_20-04\\skipskid\\templates\\front'],
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
